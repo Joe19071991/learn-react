01027e77583376bc9ba66f299bfbe81d2c7732a3
Joe19071991 <lauranove66@gmail.com>
2025-03-18 16:16:16 +0100

    création du composant App qui est lié au composant Text

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 import "./index.css";
-import { Text } from "./Text.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Text msg={"React"} />
-    <Text msg={"Js"} />
-    <Text msg={"HTML"} />
-    <Text msg={"CSS"} />
-    <Text msg={"JavaScript"} />
+    <App />
   </StrictMode>
 );
