2cb4c709723e56a17fb4411593674b80cde3e248
Joe19071991 <lauranove66@gmail.com>
2025-03-18 16:45:09 +0100

    ajout du composant text et ses props pour afficher le titre avec les firstname et le text

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import "./App.css";
 
 function App() {
   const firstname = ["Gisèle", "Benoit", "Mathilde", "Lauriane", "Gwen"];
-  return <>{firstname.map((element) => element + " ")}</>;
+  return (
+    <>
+      {firstname.map((element) => (
+        <Text key={element} msg={element} />
+      ))}
+    </>
+  );
 }
 
 export default App;
