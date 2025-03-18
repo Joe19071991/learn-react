45c779cecc8e76f0d1f37ce202c44137bd3afbcc
Joe19071991 <lauranove66@gmail.com>
2025-03-18 16:37:55 +0100

    ajout de la variablefirstname, utilisation de la méthode map pour afficher les firstname

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,8 @@ import { Text } from "./Text";
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <Text msg={"React"} />
-      <Text msg={"Js"} />
-      <Text msg={"HTML"} />
-      <Text msg={"CSS"} />
-      <Text msg={"JavaScript"} />
-    </>
-  );
+  const firstname = ["Gisèle", "Benoit", "Mathilde", "Lauriane", "Gwen"];
+  return <>{firstname.map((element) => element + " ")}</>;
 }
 
 export default App;
