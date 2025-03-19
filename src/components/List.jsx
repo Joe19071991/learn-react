6f3d21f1e2c46aa6afe3527c140fbe77885c1fc5
Joe19071991 <lauranove66@gmail.com>
2025-03-19 15:28:42 +0100

    utilisation de useState pour afficher une liste via une Api

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
+import "../App.css";
+
 export const List = () => {
+  const [todos, setTodos] = useState([]);
+
   const fetchList = async () => {
     const result = await fetch("https://jsonplaceholder.typicode.com/todos");
-    const data = result.json();
-    console.log(data);
+    const data = await result.json();
+    setTodos(data);
   };
   return (
     <div>
       <button onClick={() => fetchList()}>Fetch</button>
+      {todos.map((todo) => (
+        <p key={todo.id}>{todo.title}</p>
+      ))}
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // import App from "./components/App.jsx";
-import { Count } from "./components/Count.jsx";
+// import { Count } from "./components/Count.jsx";
+import { List } from "./components/List.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <Count />
+    {/* <Count /> */}
+    <List />
   </StrictMode>
 );
